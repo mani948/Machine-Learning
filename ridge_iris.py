3f823a0b8edea33d35dab3597e3a1d5aac45c8a6
mani948 <gudalamanikanta5@gmail.com>
2026-05-05 15:09:26 +0530

    Add files via upload

---
+++ ridge_iris.py
@@ -0,0 +1,15 @@
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Ridge
+
+# 1. Load dataset
+iris = load_iris()
+X = iris.data[:, 2].reshape(-1, 1)   # Petal length
+y = iris.data[:, 3]                  # Petal width
+
+# 2. Train Ridge Regression
+model = Ridge(alpha=1.0)
+model.fit(X, y)
+
+# 3. Predict for a sample value
+sample = [[1.5]]
+print("Predicted petal width for length 1.5:", model.predict(sample)[0])
