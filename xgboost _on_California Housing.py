88dee76007da710c97e27305b19933d5c7ad3200
mani948 <gudalamanikanta5@gmail.com>
2026-06-11 23:33:15 +0530

    Add files via upload

---
+++ xgboost _on_California Housing.py
@@ -0,0 +1,19 @@
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error
+
+# 1. Load dataset
+california = fetch_california_housing()
+X, y = california.data, california.target
+
+# 2. Split data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# 3. Train XGBoost Regression
+model = XGBRegressor(n_estimators=300, learning_rate=0.1, max_depth=6, random_state=42)
+model.fit(X_train, y_train)
+
+# 4. Predict & Evaluate
+y_pred = model.predict(X_test)
+print("Mean Squared Error:", mean_squared_error(y_test, y_pred))
